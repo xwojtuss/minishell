32fb08d7d10ee7a21ae8d218ccc2d18f1cbbf43a
Wojtek Kornatowski <wojkor338@gmail.com>
2024-07-17 20:24:09 +0200

    Readline is now a separate process

--- srcs/signals.c
+++ srcs/signals.c
@@ -5,24 +5,9 @@ Sets the global variable g_signum to the signal number, calls the handle_input f
 */
 void sig_handler(int num)
 {
-	char	*cwd;
-	char	*prompt;
-
-	g_signum = num;
-	if (num == SIGINT)
+	if (num == SIGINT || num == SIGQUIT)
 	{
-		write(1, "\n", 1);
-		// It's either just spaces or path + spaces
-		cwd = getcwd(NULL, 0);
-		if (!cwd)
-			throw_error_exit(NULL, NULL, NULL);
-		prompt = ft_strjoin(cwd, "$ ");
-		free(cwd);
-		if (!prompt)
-			throw_error_exit(NULL, NULL, NULL);
-		printf("%s", prompt);
-		// I don't know what else to do but this doesn't work fully
+		g_signum = num;
+		printf("signal received\n");	
 	}
-	else
-		handle_input(NULL);
 }
