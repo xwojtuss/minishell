a936232d572e77b9a03a34044a75e3bde44193c2
Kaletas <bkaleta64@gmail.com>
2024-08-19 00:38:10 +0200

    less issues but still there are some

--- srcs/history.c
+++ srcs/history.c
@@ -6,7 +6,7 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 18:25:00 by bkaleta           #+#    #+#             */
-/*   Updated: 2024/08/18 23:08:56 by bkaleta          ###   ########.fr       */
+/*   Updated: 2024/08/19 00:25:44 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,31 @@ void	update_last_command(t_history *history, char *new_command)
 {
 	if (history->last_cmd)
 		free(history->last_cmd);
-	history->last_cmd = strdup(new_command);
+	history->last_cmd = ft_strdup(new_command);
 	if (!history->last_cmd)
 		throw_error_exit(NULL, NULL, NULL, NULL);
 }
 
-void	free_history(t_shell *shell)
+void free_history(t_shell *shell)
 {
-	if (shell->history)
-	{
-		if (shell->history->last_cmd)
-			free(shell->history->last_cmd);
-		free(shell->history);
-	}
+    if (shell->history)
+    {
+        if (shell->history->last_cmd)
+        {
+            free(shell->history->last_cmd);
+            shell->history->last_cmd = NULL; // Uniknięcie dostępu do zwolnionej pamięci
+        }
+        free(shell->history);
+        shell->history = NULL; // Uniknięcie dostępu do zwolnionej pamięci
+    }
 }
+
+// void	free_history(t_shell *shell)
+// {
+// 	if (shell->history)
+// 	{
+// 		if (shell->history->last_cmd)
+// 			free(shell->history->last_cmd);
+// 		free(shell->history);
+// 	}
+// }
